4118cb05dac455ae3b67393869e0772d39926a48
NoWay_y <teolem77@gmail.com>
2025-07-10 21:15:14 +0200

    replace a by Link tag

--- portfolio/src/components/organisms/Navbar.tsx
+++ portfolio/src/components/organisms/Navbar.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
 
 
 export default function Navbar() {
     return (
         <div className="bg-gray-100">
             <div className="w-4/5 mx-auto flex items-center justify-between p-4 text-black border border-black rounded-b-xl">
-                <a href="/" className="text-xl font-semibold hover:underline">Téo Lemesle</a>
+                <Link href="/" className="text-xl font-semibold hover:underline">Téo Lemesle</Link>
                 <div className="space-x-4">
-                    <a href="/about" className="hover:underline">About me</a>
-                    <a href="/career" className="hover:underline">Career</a>
-                    <a href="/projects" className="hover:underline">Projects</a>
-                    <a href="/contact" className="hover:underline">Contact</a>
+                    <Link href="/about" className="hover:underline">About me</Link>
+                    <Link href="/career" className="hover:underline">Career</Link>
+                    <Link href="/projects" className="hover:underline">Projects</Link>
+                    <Link href="/contact" className="hover:underline">Contact</Link>
                 </div>
             </div>
         </div>
